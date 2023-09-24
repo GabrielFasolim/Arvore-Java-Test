daa439db0a8a8a41561666ad2462f22a0809fdb3
Gabriel Fasolim <95487111+GabrielFasolim@users.noreply.github.com>
2023-09-24 20:34:32 -0300

    Aula 21-22 Curso Java

---
+++ Arvore-Gabriel-Lima-Fasolim/aula21_arvore_binaria/src/aula21_arvore_binaria_codigo/Arvore_binaria_codigo.java
@@ -0,0 +1,64 @@
+package aula21_arvore_binaria_codigo;
+
+public class Arvore_binaria_codigo {
+
+    public static void main(String[] args) {
+        Arvore<Integer> arvore = new Arvore<Integer>();
+        arvore.adicionar(10);
+        arvore.adicionar(8);
+        arvore.adicionar(5);
+        arvore.adicionar(9);
+        arvore.adicionar(7);
+        arvore.adicionar(18);
+        arvore.adicionar(13);
+        arvore.adicionar(20);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(20);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(5);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(8);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(9);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(13);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(7);
+        arvore.remover(18);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        arvore.remover(10);
+        
+        System.out.println("\n\nEm-ordem");
+        arvore.emOrdem(arvore.getRaiz());
+        
+        //System.out.println("\n\nPré-ordem");
+        //arvore.preOrdem(arvore.getRaiz());
+        
+        //System.out.println("\n\nPós-ordem");
+        //arvore.posOrdem(arvore.getRaiz());
+        
+        
+    }
+    
+}
